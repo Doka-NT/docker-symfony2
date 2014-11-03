5e871abc46a914ca9cb001b9d839804bb2d2e0c5
root <root@research>
2014-11-03 22:26:34 +0100

    added remote debugging via -e DEBUG=1

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [ $DEBUG ]; then
+    echo "xdebug.remote_connect_back=On" >> /etc/php5/fpm/conf.d/20-xdebug.ini
+    echo "xdebug.remote_enable=On" >> /etc/php5/fpm/conf.d/20-xdebug.ini
+fi
+
 if [ -z "$1" ];
     then
     cd /var/www
